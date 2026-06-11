0a4cc196b27f93d0c2fd9b92adf56aa32fb79960
amarwalsomya-ux <amarwalsomya@gmail.com>
2026-06-11 17:20:39 +0530

    Add files via upload

---
+++ assesment2.py
@@ -0,0 +1,77 @@
+print("\n-------assisment 2---------\n")
+name=input("enter name of student:")
+print("name=",name)
+student_class=int(input("enter class of student"))
+print("class=",student_class)
+hindi=int(input("enter marks of hindi"))
+print("hindi marks:",hindi)
+php=int(input("enter marks of php"))
+print("php marks:",php)
+java=int(input("enter marks of java"))
+print("php marks:",java)
+maths=int(input("enter marks of maths"))
+print("php marks:",maths)
+cybersecurity=int(input("enter marks of cybersecurity"))
+print("php marks:",cybersecurity)
+b=hindi+php+java+maths+cybersecurity
+print("sum of all subject=",b)
+per=b*100/500
+print("percentage of student=",per)
+print("------Detail of student--------")
+print("name of student=",name)
+print("class of student=",student_class)
+print("marks of student=",b)
+print("percentage of student=",per)
+print("----string function----")
+a=input("enter name")
+print("name of student=",a)
+b=input("enter cast")
+print("cast=",b)
+c=a+" "+b
+print("conteration of string is=",c)
+d="rakesh"
+e="suresh"
+f="1233"
+print("lower=",a.lower())
+print("upper=",b.upper())
+print("titlt=",d.title())
+print("swapcase=",d.swapcase())
+print("capitalize=",d.capitalize())
+print("casefold=",e.casefold())
+print("center=",e.center(3))
+print("count=",a.count("a"))
+print("endswith=",a.endswith("a"))
+print("find=",b.find("w"))
+print("isalnum=",b.isalnum())
+print("isdigit=",b.isdigit())
+print("isnumeric=",f.isnumeric())
+print("isspace=",d.isspace())
+print("replace=",e.replace("s","k"))
+print("------assisment----")
+a=18
+b=19
+print(a<=b)
+print(b>=a)
+print(a==b)
+a+=10
+print(a)
+b-=9
+print(b)
+a*=12
+print(a)
+b/=4
+print(b)
+b%=5
+print(b)
+b//=3
+print(b)
+a!=4
+print(a)
+
+
+
+
+      
+
+
+
